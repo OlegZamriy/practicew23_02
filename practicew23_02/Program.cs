cb7ce39dda0397b651e2e9025ea7d3e62ad5f4d7
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2024-02-27 20:10:28 +0200

    1

--- practicew23_02/Program.cs
+++ practicew23_02/Program.cs
@@ -1,1 +1,31 @@
-﻿ 
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть вихідний рядок:");
+        string inputString = Console.ReadLine();
+
+        Console.WriteLine("Введіть підрядок для пошуку:");
+        string searchString = Console.ReadLine();
+
+        int occurrences = CountOccurrences(inputString, searchString);
+
+        Console.WriteLine($"Результат пошуку: {occurrences}");
+    }
+
+    static int CountOccurrences(string inputString, string searchString)
+    {
+        int count = 0;
+        int index = 0;
+
+        while ((index = inputString.IndexOf(searchString, index, StringComparison.OrdinalIgnoreCase)) != -1)
+        {
+            count++;
+            index += searchString.Length;
+        }
+
+        return count;
+    }
+}
