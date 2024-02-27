3b714807df76480381eb63965b603bb1c8988890
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2024-02-27 20:08:16 +0200

    1

--- practicew23_02/Program.cs
+++ practicew23_02/Program.cs
@@ -1,1 +1,19 @@
-﻿ 
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть речення:");
+        string sentence = Console.ReadLine();
+
+        int wordCount = CountWords(sentence);
+        Console.WriteLine($"Кількість слів у реченні: {wordCount}");
+    }
+
+    static int CountWords(string sentence)
+    {
+        string[] words = sentence.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        return words.Length;
+    }
+}
