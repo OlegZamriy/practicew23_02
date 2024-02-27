d1b515f161477a3e656669f9935ade9316a2005b
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2024-02-27 20:06:08 +0200

    1

--- practicew23_02/Program.cs
+++ practicew23_02/Program.cs
@@ -1,1 +1,39 @@
-﻿ 
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть три числа через пробіл:");
+        string input = Console.ReadLine();
+        int[] sequence = input.Split(' ').Select(int.Parse).ToArray();
+
+        Console.WriteLine("Введіть масив чисел через пробіл:");
+        input = Console.ReadLine();
+        int[] numbers = input.Split(' ').Select(int.Parse).ToArray();
+
+        int count = CountSequenceOccurrences(numbers, sequence);
+        Console.WriteLine($"Кількість повторень послідовності: {count}");
+    }
+
+    static int CountSequenceOccurrences(int[] numbers, int[] sequence)
+    {
+        int count = 0;
+        for (int i = 0; i <= numbers.Length - sequence.Length; i++)
+        {
+            bool isMatch = true;
+            for (int j = 0; j < sequence.Length; j++)
+            {
+                if (numbers[i + j] != sequence[j])
+                {
+                    isMatch = false;
+                    break;
+                }
+            }
+            if (isMatch)
+                count++;
+        }
+        return count;
+    }
+}
