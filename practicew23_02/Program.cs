82732455a5c9c26bfd9a8c762357658cd88b3271
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2024-02-27 20:09:43 +0200

    1

--- practicew23_02/Program.cs
+++ practicew23_02/Program.cs
@@ -1,1 +1,28 @@
-﻿ 
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть речення:");
+        string sentence = Console.ReadLine();
+
+        int vowelCount = CountVowels(sentence);
+        Console.WriteLine($"Кількість голосних літер у реченні: {vowelCount}");
+    }
+
+    static int CountVowels(string sentence)
+    {
+        char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+        int count = 0;
+        foreach (char c in sentence)
+        {
+            if (Array.IndexOf(vowels, c) != -1)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
