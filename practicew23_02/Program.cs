7fabb7ff7791e1738b0d37da40128dcc65a7232a
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2024-02-27 20:14:35 +0200

    1

--- practicew23_02/Program.cs
+++ practicew23_02/Program.cs
@@ -1,1 +1,42 @@
-﻿ 
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть рядок для шифрування:");
+        string originalText = Console.ReadLine();
+
+        Console.WriteLine("Введіть ключ шифрування (ціле число):");
+        int key = Convert.ToInt32(Console.ReadLine());
+
+        string encryptedText = Encrypt(originalText, key);
+        Console.WriteLine($"Зашифрований текст: {encryptedText}");
+
+        string decryptedText = Decrypt(encryptedText, key);
+        Console.WriteLine($"Розшифрований текст: {decryptedText}");
+    }
+
+    static string Encrypt(string input, int key)
+    {
+        string result = "";
+        foreach (char ch in input)
+        {
+            if (char.IsLetter(ch))
+            {
+                char shiftedChar = (char)(((ch - 'A' + key) % 26) + 'A');
+                result += shiftedChar;
+            }
+            else
+            {
+                result += ch;
+            }
+        }
+        return result;
+    }
+
+    static string Decrypt(string input, int key)
+    {
+        return Encrypt(input, 26 - key);
+    }
+}
