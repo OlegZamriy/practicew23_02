7da29860d30a5818be145f559d67cc030918a337
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2024-02-27 20:04:45 +0200

    1

--- practicew23_02/Program.cs
+++ practicew23_02/Program.cs
@@ -1,1 +1,25 @@
-﻿ 
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 9 };
+
+        CountAndDisplay(numbers);
+    }
+
+    static void CountAndDisplay(int[] numbers)
+    {
+        int evenCount = numbers.Count(num => num % 2 == 0);
+
+        int oddCount = numbers.Count(num => num % 2 != 0);
+
+        int uniqueCount = numbers.Distinct().Count();
+
+        Console.WriteLine($"Кількість парних чисел: {evenCount}");
+        Console.WriteLine($"Кількість непарних чисел: {oddCount}");
+        Console.WriteLine($"Кількість унікальних елементів: {uniqueCount}");
+    }
+}
