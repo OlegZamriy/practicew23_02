08cc2d1f94add86ddeae80aa82d12a00493f2b84
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2024-02-27 20:05:25 +0200

    1

--- practicew23_02/Program.cs
+++ practicew23_02/Program.cs
@@ -1,1 +1,21 @@
-﻿ 
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        Console.WriteLine("Введіть число, порівняння з яким ви хочете зробити:");
+        int threshold = Convert.ToInt32(Console.ReadLine());
+
+        int count = CountValuesBelowThreshold(numbers, threshold);
+        Console.WriteLine($"Кількість значень у масиві менше {threshold}: {count}");
+    }
+
+    static int CountValuesBelowThreshold(int[] numbers, int threshold)
+    {
+        int count = numbers.Count(num => num < threshold);
+        return count;
+    }
+}
