41a60e62db87993e060bb4196b6467ed027fe6fe
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2024-02-27 20:17:09 +0200

    1

--- practicew23_02/Program.cs
+++ practicew23_02/Program.cs
@@ -1,1 +1,52 @@
-﻿ 
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть арифметичний вираз (тільки + і -):");
+        string input = Console.ReadLine();
+
+        double result = CalculateExpression(input);
+        Console.WriteLine("Результат: " + result);
+    }
+
+    static double CalculateExpression(string input)
+    {
+        double result = 0;
+        char currentOperator = '+';
+        int currentIndex = 0;
+
+        while (currentIndex < input.Length)
+        {
+            int nextOperatorIndex = input.IndexOfAny(new char[] { '+', '-' }, currentIndex);
+            if (nextOperatorIndex == -1)
+            {
+                nextOperatorIndex = input.Length;
+            }
+
+            double operand = double.Parse(input.Substring(currentIndex, nextOperatorIndex - currentIndex));
+
+            if (currentOperator == '+')
+            {
+                result += operand;
+            }
+            else if (currentOperator == '-')
+            {
+                result -= operand;
+            }
+
+            if (nextOperatorIndex < input.Length)
+            {
+                currentOperator = input[nextOperatorIndex];
+                currentIndex = nextOperatorIndex + 1;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return result;
+    }
+}
