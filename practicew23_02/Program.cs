66b8592458df0851584e8982394121213e8c9587
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2024-02-27 20:08:56 +0200

    1

--- practicew23_02/Program.cs
+++ practicew23_02/Program.cs
@@ -1,1 +1,28 @@
-﻿ 
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть речення:");
+        string sentence = Console.ReadLine();
+
+        string[] words = sentence.Split(' ');
+
+        Console.WriteLine("Після перевороту:");
+        foreach (string word in words)
+        {
+            string reversedWord = ReverseString(word);
+            Console.Write(reversedWord + " ");
+        }
+    }
+
+    static string ReverseString(string str)
+    {
+        char[] charArray = str.ToCharArray();
+
+        Array.Reverse(charArray);
+
+        return new string(charArray);
+    }
+}
