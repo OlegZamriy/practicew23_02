5d817a7bbfb24b9be91cbea459c98093ff01b5b5
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2024-02-27 20:06:51 +0200

    1

--- practicew23_02/Program.cs
+++ practicew23_02/Program.cs
@@ -1,1 +1,35 @@
-﻿ 
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array1 = { 1, 2, 3, 4, 5 };
+        int[] array2 = { 3, 4, 5, 6, 7 };
+
+        HashSet<int> uniqueElements = new HashSet<int>();
+
+        foreach (int num in array1)
+        {
+            uniqueElements.Add(num);
+        }
+
+        foreach (int num in array2)
+        {
+            if (!uniqueElements.Contains(num))
+            {
+                uniqueElements.Add(num);
+            }
+        }
+
+        int[] combinedArray = new int[uniqueElements.Count];
+        uniqueElements.CopyTo(combinedArray);
+
+        Console.WriteLine("Загальні елементи двох масивів без повторень:");
+        foreach (int num in combinedArray)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
